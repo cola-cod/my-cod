b6c26e1f216305ac0abbe27725d8085f1ecb6155
cola-cod <133337461+cola-cod@users.noreply.github.com>
2023-05-17 14:40:07 +0800

    Add files via upload

---
+++ daycod1/cod1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	//printf("Hello World");
+	//return 0;
+	int a = 20;
+	double weight = 65.5;
+	float large = 12.3;
+	int c = a + 1;
+	printf("%d\n", c);
+	printf("%lf\n", weight);
+	printf("%f\n", large);
+	char ch = 'a';
+	printf("%d\n",sizeof(char));
+	printf("你好\n");
+	printf("nihao\n");
+	printf("%d", sizeof(long));
+	//sizeof 关键字-操作符-计算类型或者变量所占空间大小
+	
+	return 0;
+
+
+
+
+
+}
---
+++ define宏和常量/define宏定义.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//#define MAX 1000
+//int main()
+//{
+//
+	//printf("%d\n", MAX);
+	//return 0;
+//}
+//#define ADD(x,y) x+y
+#define ADD(x,y) ((x)+(y))
+int main()
+{
+	
+	printf("%d\n", 4 * ADD(2, 3));
+	return 0;
+}
